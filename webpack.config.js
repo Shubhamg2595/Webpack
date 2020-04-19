49e6681fbcf6d08dd351ca59ce7cf080e57d439f
Shubham Gupta <shubhamg2595@gmail.com>
2020-04-19 13:16:23 +0530

    cleanUp buildFiles using clean-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -35,6 +36,14 @@ module.exports = {
         new TerserPlugin(),
         new MiniCssExtractPlugin({
             filename: 'styles.[contenthash].css'
-        })
+        }),
+        new CleanWebpackPlugin(
+            {
+                cleanOnceBeforeBuildPatterns: [
+                    '**/*',
+                    path.join(process.cwd(),'build/**/*')
+                ]
+            }
+        )
     ]
 }   
